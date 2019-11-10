2d456497528897bc6182424c018832bdaa2e2f45
laiove <1679852974@qq.com>
2019-11-10 15:21:21 +0800

    firstWeb

---
+++ firstWeb/php/ajaxTest.php
@@ -0,0 +1,22 @@
+<?php
+header('Content-Type: application/json');
+?>
+
+{
+  "data":[{
+       "name":"上海",
+       "id":3,
+       "speak":"好好读书，天天向上"
+       },
+       {
+              "name":"上海",
+              "id":3,
+              "speak":"好好读书，天天向上"
+              },
+              {
+                     "name":"上海",
+                     "id":3,
+                     "speak":"好好读书，天天向上"
+                     }
+        ]
+}
---
+++ firstWeb/php/imgTalk.php
@@ -0,0 +1,23 @@
+<?php
+header('Content-Type: application/json');
+?>
+
+{
+  "data":[{
+       "name":["名胜古迹天天有","北京"],
+       "src":"./images/l1.jpg"
+       },
+       {
+       "name":["体会相亲公园","上海"],
+       "src":"./images/l2.jpg"
+        },
+       {
+        "name":["美食十条该","广东"],
+        "src":"./images/l3.jpg"
+         },
+         {
+        "name":["高科技发展源","深圳"],
+        "src":"./images/l4.jpg"
+         }
+        ]
+}
